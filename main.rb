1f41abdfa6a4a50d170f9a5f9e663bc1af3f4f77
Saad Bakht <rotk13@gmail.com>
2014-01-19 14:05:28 -0500

    Displays total time at the end

--- main.rb
+++ main.rb
@@ -1,12 +1,17 @@
+totalTime = 0
 File.open(ARGV[0]).each_line do |line|
 	puts line
 	t1 = Time.now
-	a = STDIN.gets.chomp
+	input = STDIN.gets.chomp
 	t2 = Time.now
-	puts t2 - t1
-	if a == line.strip
+
+	timeTaken = t2 - t1
+	totalTime += timeTaken
+	puts "Time Taken: #{timeTaken}"
+	if input == line.strip
 		puts "CORRECT"
 	else
 		puts "WRONG"
 	end
 end
+puts "Total Time: #{totalTime}"
