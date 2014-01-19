a41fe9c34c7509960710c59edc6f431a7c99be05
Saad Bakht <rotk13@gmail.com>
2014-01-19 14:12:17 -0500

    Displays word and character count

--- main.rb
+++ main.rb
@@ -1,17 +1,31 @@
 totalTime = 0
+totalWordCount = 0
+totalCharCount = 0
 File.open(ARGV[0]).each_line do |line|
 	puts line
 	t1 = Time.now
 	input = STDIN.gets.chomp
 	t2 = Time.now
 
+	wordCount = line.count(' ') + 1
+	totalWordCount += wordCount
+	puts "Word Count: #{wordCount}"
+
+	charCount = line.length
+	totalCharCount += charCount
+	puts "Character Count: #{charCount}"
+
 	timeTaken = t2 - t1
 	totalTime += timeTaken
-	puts "Time Taken: #{timeTaken}"
+	puts "Time Taken: #{timeTaken.round(2)}"
+
 	if input == line.strip
 		puts "CORRECT"
 	else
 		puts "WRONG"
 	end
+	puts "\n"
 end
-puts "Total Time: #{totalTime}"
+puts "Total Word Count: #{totalWordCount}"
+puts "Total Character Count: #{totalCharCount}"
+puts "Total Time: #{totalTime.round(2)}"
